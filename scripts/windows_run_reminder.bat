e5aa16e58bdc38bbcc06300e8c90a40886364b89
Marc Susagna <marcsusagna@gmail.com>
2025-03-03 20:23:00 +0100

    fix scripts

--- scripts/windows_run_reminder.bat
+++ scripts/windows_run_reminder.bat
@@ -1,5 +1,4 @@
 @echo off
-:: Assumes executed from scripts/ folder
-cd .. && ^
-poetry install && ^
-poetry run remind_me
+start cmd /k wsl bash -c "echo 'Starting app' && cd .. && poetry install && poetry run start_app"
+timeout /t 5
+start cmd /k wsl bash -c "echo 'Running reminders' && cd .. && poetry run remind_me --seconds=5"
