d4ed138c85c4db71a9f25feb446b9dd3d36893ef
Marc Susagna <marcsusagna@gmail.com>
2025-03-03 20:33:25 +0100

    fixed windows executable

--- scripts/windows_run_reminder.bat
+++ scripts/windows_run_reminder.bat
@@ -1,4 +1,6 @@
 @echo off
-start cmd /k wsl bash -c "echo 'Starting app' && cd .. && poetry install && poetry run start_app"
+cd /d ".."
+poetry install
+start cmd /k "poetry run start_app"
 timeout /t 5
-start cmd /k wsl bash -c "echo 'Running reminders' && cd .. && poetry run remind_me --seconds=5"
+start cmd /k "poetry run remind_me --seconds=900"
--- scripts/wsl_run_reminder.bat
+++ scripts/wsl_run_reminder.bat
@@ -1,4 +1,4 @@
 @echo off
 start cmd /k wsl bash -c "echo 'Starting app' && source ~/.profile && cd reminder && poetry install && poetry run start_app"
 timeout /t 5
-start cmd /k wsl bash -c "echo 'Running reminders' && source ~/.profile && cd reminder && poetry install && poetry run remind_me --seconds=5"
+start cmd /k wsl bash -c "echo 'Running reminders' && source ~/.profile && cd reminder && poetry install && poetry run remind_me --seconds=900"
